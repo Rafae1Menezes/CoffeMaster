05fcd39d792d3b7f227498beb6aa80e8903faede
Rafael Menezes <menezes.inbox@gmail.com>
2023-03-07 10:17:52 -0300

    feat: commit initial

--- CoffeMaster/ContentView.swift
+++ CoffeMaster/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+   
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Greeting()
+            Greeting()
         }
-        .padding()
+    }
+}
+
+struct Greeting: View {
+    @State var name = "rafael"
+    
+    var body: some View {
+        VStack {
+            TextField("Digite seu nome", text: $name)
+            Text("hello \(name)")
+        }
     }
 }
 
