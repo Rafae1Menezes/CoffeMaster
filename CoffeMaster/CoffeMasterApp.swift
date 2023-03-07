ea63459b83be0215a8cd510eebac750916f0634d
Rafael Menezes <menezes.inbox@gmail.com>
2023-03-07 09:09:09 -0300

    Initial Commit

---
+++ CoffeMaster/CoffeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMasterApp.swift
+//  CoffeMaster
+//
+//  Created by Rafael Menezes on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
